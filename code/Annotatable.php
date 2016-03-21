ec538708ae484cfa4ccbe519aae16566aa611d0e
Simon <simon@Kaitlyn.local>
2016-03-21 18:56:59 +1300

    Forgot to remove debug statement.

--- code/Annotatable.php
+++ code/Annotatable.php
@@ -8,6 +8,8 @@
 class Annotatable extends DataExtension
 {
     /**
+     * This is the base function on which annotations are started.
+     * 
      * @todo rewrite this. It's not actually a requireDefaultRecords. But it's the only place to hook into the build-process to start the annotation process.
      * @return bool
      */
@@ -17,8 +19,6 @@ class Annotatable extends DataExtension
         if (!Config::inst()->get('DataObjectAnnotator', 'enabled') || $skipAnnotation !== null) {
             return false;
         }
-
-        print_r($this->owner->Classname . "\n");
 
         /* @var $annotator DataObjectAnnotator */
         $annotator = DataObjectAnnotator::create();
